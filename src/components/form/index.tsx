df5863bc03c7d11b89aafff72e3d62972d7255f4
Afonso <afonsobezerra199@gmail.com>
2023-03-06 12:46:03 -0300

    config Form Cv

--- src/components/form/index.tsx
+++ src/components/form/index.tsx
@@ -1,6 +1,34 @@
+import { useState } from 'react';
+import { fetcher } from '@services/fetchers';
+import { phoneMask } from './masks';
 import { FormStyle } from './style';
 
 const Form = () => {
+  const [value, setValue] = useState('');
+  function submit(values: any) {
+    const dataCvCRM = {
+      nome: values.target.name.value,
+      email: values.target.email.value,
+      telefone: values.target.tel.value,
+      idempreendimento: 3,
+      origem: 'SI',
+      midia: 'Google LP',
+      conversao: 'CoreAg',
+    };
+    return fetcher
+      .post('https://julioejulio.cvcrm.com.br/api/cvio/lead', dataCvCRM)
+      .then((res) => {
+        alert(
+          'Recebemos seu formulário.\n\n Entraremos em contato com você em breve',
+        );
+        return res;
+      })
+      .catch(() =>
+        alert(
+          'Ops... Houve um erro ao enviar seu formulário.\n\n Por favor tente novamente mais tarde.',
+        ),
+      );
+  }
   return (
     <FormStyle id="form">
       <div className="header">
@@ -9,7 +37,12 @@ const Form = () => {
         <span>m²</span> com parcelas a partir de <span>R$</span>
         <strong>619</strong>,00*
       </div>
-      <form>
+      <form
+        onSubmit={(ev) => {
+          ev.preventDefault();
+          submit(ev);
+        }}
+      >
         <label htmlFor="name">
           <strong>Nome</strong>
           <input
@@ -30,7 +63,16 @@ const Form = () => {
         </label>
         <label htmlFor="tel">
           <strong>Telefone</strong>
-          <input type="tel" name="tel" id="tel" placeholder="21 9988-7766" />
+          <input
+            type="tel"
+            name="tel"
+            id="tel"
+            placeholder="(21) 9988-7766"
+            value={value}
+            onChange={({ target }) => {
+              setValue(phoneMask(target.value));
+            }}
+          />
         </label>
         <button type="submit">Quero saber mais</button>
       </form>
