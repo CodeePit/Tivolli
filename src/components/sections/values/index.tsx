6e13f2ea2591ccf993a730450fe9cde0a27f09ee
Afonso <84323490+AfonsoBezerra@users.noreply.github.com>
2024-12-05 15:31:06 -0300

    Update index.tsx

--- src/components/sections/values/index.tsx
+++ src/components/sections/values/index.tsx
@@ -8,8 +8,9 @@ const ValuesSection = () => {
           O Loteamento fechado que cabe no seu bolso!
         </div>
         <div className="values">
-          A partir de R$<span>79.500,00</span> em até <span>36x</span> sem
+          A partir de R$<span>79.500,00</span> em até <span>42x</span> sem
           juros! Consulte condições.
+          <p>* condições válidas até a data de 31/12/2024.</p>
         </div>
         <a href="#form">Falar com um especialista</a>
       </div>
