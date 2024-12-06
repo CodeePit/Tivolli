7ec099a6aee77989ab25f781c3a440bc6055e3b4
Afonso <84323490+AfonsoBezerra@users.noreply.github.com>
2024-12-06 13:27:12 -0300

    Update index.tsx

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -4,11 +4,8 @@ const Footer = () => {
   return (
     <FooterStyle>
       <span className="values">
-        Valor à vista de R$ 89.900 mediante a transferência bancária ou pix
-        válido para lote J-02. Parcelamento em até 48x com entrada mínima
-        de 35% do valor total, sem juros com IPCA. Parcelas de R$619,00 com
-        entrada 8%, mais 12 parcelas anuais de R$750,00 em cima do valor
-        financiado pela loteadora, com juros e correções.
+        Valor à vista a partir de R$ 89.900,00 ou em até 42 meses sem juros e sem correção , consulte as condições especiais, promoção válida até 31/12/2024.
+        Lote de referência para condição promocional Quadra J lote 02
       </span>
       <span className="line" />
       <a href="https://coreag.com.br/">Intelligence By Core.Ag</a>
