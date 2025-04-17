c63c978cd8ee097b92d08c3d7ba3ec42f6c2755b
Afonso <84323490+AfonsoBezerra@users.noreply.github.com>
2025-04-17 11:49:57 -0300

    Update index.tsx

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -3,10 +3,10 @@ import { FooterStyle } from './style';
 const Footer = () => {
   return (
     <FooterStyle>
-      <span className="values">
+{/*       <span className="values">
         Valor à vista a partir de R$ 89.900,00 ou em até 42 meses sem juros e sem correção , consulte as condições especiais, promoção válida até 31/12/2024.
         Lote de referência para condição promocional Quadra J lote 02
-      </span>
+      </span> */}
       <span className="line" />
       <a href="https://coreag.com.br/">Intelligence By Core.Ag</a>
     </FooterStyle>
